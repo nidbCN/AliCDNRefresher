6624e4c869b38a96573526e1bfce26ace443a4d1
Gaein nidb <mail@gaein.cn>
2021-04-07 18:51:07 +0800

    Update script

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
-# !/bin/bash
+#/bin/bash
+if [ ! -d "/Release" ]; then
+    echo "\tNew Release Directory."
+    mkdir /Release
+else
+    echo "\tClean Release Directory."
+    rm -rf /Release/*
+fi
 
 dotnet publish -c Release --no-self-contained -r ubuntu.20.04-x64 -o ./Release 
